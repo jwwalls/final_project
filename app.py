cabd0dc137d5eb9b220c343affc6e5193df5708c
jwwalls <121780225+jwwalls@users.noreply.github.com>
2023-03-29 11:41:06 -0500

    cleanup

--- app.py
+++ app.py
@@ -1,41 +1,11 @@
-import os
-import csv
-import math
-import time
-
-import sqlite3 as SQ
-from flask import Flask, flash, redirect, render_template, request, session, jsonify
+from flask import Flask, render_template, request
 from flask_session import Session
-from tempfile import mkdtemp
-from werkzeug.security import check_password_hash, generate_password_hash
-
-from helpers import apology, login_required, lookup, usd
-
 # Configure application
 app = Flask(__name__)
-current_time = time.ctime()
-# Custom filter
-app.jinja_env.filters["usd"] = usd
-
-# Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-app.jinja_env.auto_reload = True
-app.config['TEMPLATES_AUTO_RELOAD'] = True
 Session(app)
 
 
-@app.after_request
-def after_request(response):
-    """Ensure responses aren't cached"""
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
-
-
 @app.route("/", methods=["GET", "POST"])
-
 def index():  
     return render_template("index.html")
 
@@ -54,7 +24,6 @@ def array():
         return render_template("array.html", array=return_array)
     
 @app.route("/linked_list", methods=["GET", "POST"])
-
 def linked_list():
     if request.method == "GET":        
         return render_template("linked_list.html", array=[2,1,3])
@@ -70,7 +39,6 @@ def linked_list():
     
 
 @app.route("/grid", methods=["GET", "POST"])
-
 def grid():
     if request.method == "GET":
         
@@ -85,8 +53,6 @@ def grid():
             return_array.append(row)
    
         return render_template("grid.html", grid=return_array)
-
-
 
 @app.route("/binary_tree", methods=["GET", "POST"])
 def binary_tree():
@@ -113,8 +79,6 @@ def binary_tree():
         connections_index = [[indices_dict[node] for node in connection] for connection in return_array]              
 
         return render_template("binary_tree.html", array=unique_vals, connections=connections_index)
-
-
 
 @app.route("/graph", methods=["GET", "POST"])
 def graph():
